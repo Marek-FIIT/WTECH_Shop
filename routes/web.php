ffef699644083dd33b7fbdb2cdb7bf883cf457ac
xkromkad <71709420+xkromkad@users.noreply.github.com>
2021-11-16 23:56:36 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Models\FirstLevelCategory;
 use App\Models\SecondLevelCategory;
 use App\Models\ThirdLevelCategory;
+use App\Http\Controllers\BrandsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,5 @@ use App\Models\ThirdLevelCategory;
 Route::get('/', function () {
     return view('index');//->with('categories', $categories);
 });
+
+Route::resource('brands', BrandsController::class);
