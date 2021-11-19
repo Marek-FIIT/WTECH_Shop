4836b2efd3496d003b40879267826ca8b9ab19ad
Marek-FIIT <55746905+Marek-FIIT@users.noreply.github.com>
2021-11-19 13:36:06 +0100

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,16 +7,21 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/products_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/homepage_style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/navfoot_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/product_style.css') }}">
     <script src="{{ asset('js/Login_Register_swap.js') }}"></script>
     @yield('scripts')
 </head>
 <body>
 @include('common.header')
 @include('common.nav')
-<main>
-    @yield('main')
-</main>
+
+@yield('main')
+
 @include('common.modal')
 @include('common.footer')
 </body>
