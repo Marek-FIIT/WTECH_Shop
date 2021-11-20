45bb83fb95ae6e3c4dee5cc71ae8ff4f6d4cbb62
Marek-FIIT <55746905+Marek-FIIT@users.noreply.github.com>
2021-11-20 21:05:13 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\FirstLevelCategory;
-use App\Models\SecondLevelCategory;
-use App\Models\ThirdLevelCategory;
-use App\Http\Controllers\BrandsController;
 
 
 /*
@@ -18,8 +14,26 @@ use App\Http\Controllers\BrandsController;
 |
 */
 
+use App\Http\Controllers\CategoryController;
+Route::resource('categories', CategoryController::class);
+
+use App\Http\Controllers\ProductController;
+Route::resource('products', ProductController::class);
+
 Route::get('/', function () {
+    /*$categories = array();//[][][] = "000";
+    $firstLevelCategories = FirstLevelCategory::all();
+
+    for ($i = 0; $i < sizeof($firstLevelCategories); $i++) {
+        print($firstLevelCategories[$i]->name);
+        $categories[$firstLevelCategories[$i]->name] = array();
+        $secondLevelCategories = SecondLevelCategory::where('1st_level_category_id', $firstLevelCategories[$i]->id)->get();
+
+        for ($j = 0; $j < sizeof($secondLevelCategories); $j++) {
+            $categories[ $firstLevelCategories[$i]->name]
+            [$secondLevelCategories[$j]->name] = ThirdLevelCategory::where('2nd_level_category_id', $secondLevelCategories[$j]->id)->get();
+        }
+    }*/
+
     return view('index');//->with('categories', $categories);
 });
-
-Route::resource('brands', BrandsController::class);
