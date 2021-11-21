918d52265f584c7844d8b45ae40d8ab7086ab726
xkromkad <71709420+xkromkad@users.noreply.github.com>
2021-11-21 16:25:30 +0100

    Add files via upload

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.start')
 
 @section('title', 'Brands')
 
@@ -13,15 +13,15 @@
             <!--sidebar-->
             <aside class="d-none d-md-block col-xl-2 col-lg-3 col-md-4">
                 <div class="sidebar">
-                    <a href="#" class="sidebar-title"><h5>{{$data['name']}}</h5></a>
+                    <a href="#" class="sidebar-title"><h5>{{$data['sidebarTitle']}}</h5></a>
                     <ul class="sidebar-items">
                         @foreach($data['brandslist'] as $brand)
-                            <li><a class="item" href="#">{{ $brand->name }}</a></li>
+                            <li><a class="item" href="{{ url('brands', $brand->id ) }}">{{ $brand->name }}</a></li>
                         @endforeach
                     </ul>
                 </div>
             </aside>
-            <!--products-->
+            <!--brands-->
             <section class="col-xl-10 col-lg-9 col-md-8">
                 <h1>{{$data['name']}}</h1>
                 <!--filters-->
@@ -57,7 +57,7 @@
                                 Veľkosť
                             </button>
                             <div class="brand dropdown-menu" aria-labelledby="size-selection">
-                                <form class=" color-list mx-2">
+                                <form class=" size-list mx-2">
                                     <input type="checkbox" id="xs" value="xs">
                                     <label for="xs">XS</label><br>
                                     <input type="checkbox" id="s" value="s">
@@ -77,58 +77,48 @@
                             </button>
                             <div class="brand dropdown-menu pre-scrollable" aria-labelledby="color-selection">
                                 <form class=" color-list mx-2">
-                                    <input type="checkbox" id="red" value="red">
-                                    <label for="red">Červená</label><br>
-                                    <input type="checkbox" id="black" value="black">
-                                    <label for="black">Čierna</label><br>
-                                    <input type="checkbox" id="yellow" value="yellow">
-                                    <label for="yellow">Žltá</label><br>
-                                    <input type="checkbox" id="blue" value="blue">
-                                    <label for="blue">Modrá</label><br>
-                                    <input type="checkbox" id="white" value="white">
-                                    <label for="white">Biela</label><br>
-                                    <input type="checkbox" id="pink" value="pink">
-                                    <label for="pink">Ružová</label><br>
+                                    @foreach($data['colorsList'] as $color)
+                                        <input type="checkbox" id="{{$color->id}}" value="{{$color->hexadecimal}}">
+                                        <label for="{{$color->id}}">{{$color->name}}</label><br>
+                                    @endforeach
                                 </form>
                             </div>
                         </div>
+                        @if ($data['name'] != 'Brands')
                         <div class="dropdown mr-3 mb-2">
                             <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Značka
                             </button>
                             <div class="brand dropdown-menu pre-scrollable" aria-labelledby="brand-selection">
                                 <form class=" brand-list mx-2">
-                                    <input type="checkbox" id="brand 1" value="adidas">
-                                    <label for="brand 1">Adidas</label><br>
-                                    <input type="checkbox" id="brand 2" value="nike">
-                                    <label for="brand 2">Nike</label><br>
-                                    <input type="checkbox" id="brand 3" value="puma">
-                                    <label for="brand 3">Puma</label><br>
-                                    <input type="checkbox" id="brand 4" value="Gucci">
-                                    <label for="brand 4">Gucci</label><br>
-                                    <input type="checkbox" id="brand 5" value="the north face">
-                                    <label for="brand 5">The North Face</label><br>
+                                    @foreach($data['brandslist'] as $brand)
+                                        <input type="checkbox" id="{{$brand->id}}" value="{{$brand->name}}">
+                                        <label for="{{$brand->id}}">{{$brand->name}}</label><br>
+                                    @endforeach
                                 </form>
                             </div>
                         </div>
+                        @endif
                     </div>
                 </div>
                 <!--all product on the page-->
                 <div class="row">
                     <!--Product -->
-                    <div class="product col-sm-12 col-md-6 col-lg-4 col-xl-3">
-                        <a href="#" class="single-product">
-                            <div class="wish-icon">
-                                <img class="product-img w-100" src="../sources/klobuk1.jpg" alt="Klobuk">
-                                <button class="btn"><img src="../sources/bookmark.svg" alt="bookmark" width="35" height="35"></button>
-                            </div>
-                            <div class="product-info">
-                                <h3 class="product-title">Autumn&style štýlový klobúk</h3>
-                                <h4 class="product-old-price">15 €</h4>
-                                <h4 class="product-price">7,50 €</h4>
-                            </div>
-                        </a>
-                    </div>
+                    @foreach($data['products'] as $product)
+                        <div class="product col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                            <a href="/products/{{ $product->id }}" class="single-product">
+                                <div class="wish-icon">
+                                    <img class="product-img w-100" src="{{ asset('images/'.$product->src_image) }}" alt="Klobuk">
+                                    <button class="btn"><img src="{{ asset('images/bookmark.svg') }}" alt="bookmark" width="35" height="35"></button>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-title">{{$product->name}}</h3>
+                                    <h4 class="product-old-price"></h4>
+                                    <h4 class="product-price">{{$product->price}} €</h4>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
             </section>
         </div>
